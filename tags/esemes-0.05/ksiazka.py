10c1821958e2055352d28f1f56572d5b22e166dc
skrobul <skrobul@a297add9-8d02-0410-80ee-bbb8238a63cf>
2005-10-13 20:27:50 +0000

    Dodano: 	- obsluga Ery 	- prosty instalator 	- ksiazke telefoniczna

---
+++ tags/esemes-0.05/ksiazka.py
@@ -0,0 +1,56 @@
+# -*- coding: iso-8859-2 -*-
+
+import os,re 
+
+class KsiazkaTelefoniczna:
+	def __init__(self):
+		self.ksiazka = {}
+		self.otworzplik('r+')
+		self.przeparsuj()
+
+	def otworzplik(self, tryb):
+		bpath = os.path.expanduser('~/.smsaddr')
+		try:
+			self.bookfile = open(bpath, tryb)
+		except IOError:
+			print 'Nie mo¿na otworzyæ pliku ksi±¿ki telefonicznej. Tworze pusta ksiazke tel.'
+			self.bookfile = open(bpath, "w")
+			
+	def __destroy__(self):
+		if self.bookfile:
+			print 'Zamknalem plik'
+			self.bookfile.close()
+
+	def przeparsuj(self):
+		for linia in self.bookfile:
+			wpis = re.match("^([^0-9]+):(\d+)$", linia)
+			if wpis and not self.podajnumer(wpis.group(1)):
+				nazwa = wpis.group(1)
+				numer = wpis.group(2)
+				self.ksiazka[nazwa] = numer
+	
+	def podajnumer(self, nazwa):
+		try:
+			numer = self.ksiazka[nazwa]
+			return numer
+		except KeyError:
+			return None
+
+	def zapisz(self):
+			self.otworzplik("w")
+			for nazwa in self.ksiazka:
+				self.bookfile.write(nazwa + ':' + self.ksiazka[nazwa] + '\n') 
+			self.otworzplik("r")
+
+	def usun(self, co):
+		if self.ksiazka.has_key(co):
+			del self.ksiazka[co]
+			self.zapisz()
+
+	def dodaj(self, nazwa, numer):
+		if numer.isdigit() and len(numer) == 9 and not self.podajnumer(nazwa):
+			self.ksiazka[nazwa] = numer
+			self.zapisz()
+
+
+# vim: ts=2	
