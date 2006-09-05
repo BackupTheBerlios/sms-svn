7261c7ca0279f7dc17f95edb0ba95a82517bdd41
skrobul <skrobul@a297add9-8d02-0410-80ee-bbb8238a63cf>
2006-09-05 09:18:24 +0000

    poprawiona obsluga orange

--- trunk/orange.py
+++ trunk/orange.py
@@ -18,7 +18,7 @@ class OrangeSMS: # wysylanie do sieci Orange
 		opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
 		# orange index#{{{
 		request = urllib2.Request(baseURLSSL)
-		request.add_header('User-Agent', 'Opera/8.00 (Windows NT 5.0; U; en)')
+		request.add_header('User-Agent', 'Opera/9.00 (Windows NT 5.0; U; en)')
 		try:
 		    result = opener.open(request)
 		    if self.debug:
@@ -37,31 +37,33 @@ class OrangeSMS: # wysylanie do sieci Orange
 		    print 'Connection with https://www.orange.pl/portal/map/map failed.'#}}}
 		
 		# orange login#{{{
-		parmdicta = {'_dyncharset': 'UTF-8',
-		             '/amg/ptk/map/core/formhandlers/AdvancedProfileFormHandler.loginErrorURL': '/portal/map/map/signin',
-		             '_D:/amg/ptk/map/core/formhandlers/AdvancedProfileFormHandler.loginErrorURL': ' ',
-		             '/amg/ptk/map/core/formhandlers/AdvancedProfileFormHandler.loginSuccessURL': 'http://www.orange.pl/portal/map/map/pim',
-		             '_D:/amg/ptk/map/core/formhandlers/AdvancedProfileFormHandler.loginSuccessURL': ' ',
-		             '/amg/ptk/map/core/formhandlers/AdvancedProfileFormHandler.value.login': self.login,
-		             '_D:/amg/ptk/map/core/formhandlers/AdvancedProfileFormHandler.value.login': ' ',
-		             '/amg/ptk/map/core/formhandlers/AdvancedProfileFormHandler.value.password': self.password,
-		             '_D:/amg/ptk/map/core/formhandlers/AdvancedProfileFormHandler.value.password': ' ',
-		             '/amg/ptk/map/core/formhandlers/AdvancedProfileFormHandler.login.x': '0',
-		             '/amg/ptk/map/core/formhandlers/AdvancedProfileFormHandler.login.y': '0',
-		             '_D:/amg/ptk/map/core/formhandlers/AdvancedProfileFormHandler.login': ' ',
-		             '_DARGS': '/gear/static/login.jsp'}
-		
-		request = urllib2.Request(baseURLSSL + '/portal/map/map/home?_DARGS=/gear/static/login.jsp')
+		parmdicta = {
+                 '_dyncharset' : 'UTF-8',
+                 '/amg/ptk/map/core/formhandlers/AdvancedProfileFormHandler.loginErrorURL' : '/portal/map/map/signin',
+                 '_D:/amg/ptk/map/core/formhandlers/AdvancedProfileFormHandler.loginErrorURL' : ' ',
+                 '/amg/ptk/map/core/formhandlers/AdvancedProfileFormHandler.loginSuccessURL' : 'http://www.orange.pl/portal/map/map/pim',
+                 '_D:/amg/ptk/map/core/formhandlers/AdvancedProfileFormHandler.loginSuccessURL' : ' ',
+                 '/amg/ptk/map/core/formhandlers/AdvancedProfileFormHandler.value.login' : self.login,
+                 '_D:/amg/ptk/map/core/formhandlers/AdvancedProfileFormHandler.value.login' : ' ',
+                 '/amg/ptk/map/core/formhandlers/AdvancedProfileFormHandler.value.password' : self.password,
+                 '_D:/amg/ptk/map/core/formhandlers/AdvancedProfileFormHandler.value.password' : ' ',
+                 '_D:/amg/ptk/map/core/formhandlers/AdvancedProfileFormHandler.login' : ' ',
+                 '_DARGS' : '/gear/static/home/login.jsp.loginFormId',
+                 '/amg/ptk/map/core/formhandlers/AdvancedProfileFormHandler.login' : ' ',
+                 '/amg/ptk/map/core/formhandlers/AdvancedProfileFormHandler.login.x' : '5',
+                 '/amg/ptk/map/core/formhandlers/AdvancedProfileFormHandler.login.y' : '5'}
+		request = urllib2.Request(baseURLSSL + '/portal/map/map/homeo?_DARGS=/gear/static/home/login.jsp.loginFormId')
 		postdata = urllib.unquote(urllib.urlencode(parmdicta))
 		request.add_data(postdata)
-		request.add_header('User-Agent', 'Opera/8.00 (Windows NT 5.0; U; en)')
+		print postdata
+		request.add_header('User-Agent', 'Opera/8.10 (Windows NT 5.0; U; en)')
 		try:
 		    result = opener.open(request)
 		    if self.debug:
 		        print 'Logged'
 		except IOError:
 		    print 'Not logged'#}}}
-		
+	  	
 		# orange SMS form#{{{
 		request = urllib2.Request(baseURL + '/portal/map/map/message_box?mbox_view=newsms&mbox_edit=new')
 		request.add_header('User-Agent', 'Opera/8.00 (Windows NT 5.0; U; en)')
@@ -82,14 +84,15 @@ class OrangeSMS: # wysylanie do sieci Orange
 		             '/amg/ptk/map/messagebox/formhandlers/MessageFormHandler.successURL': '/portal/map/map/message_box?mbox_view=messageslist',
 		             '_D:/amg/ptk/map/messagebox/formhandlers/MessageFormHandler.successURL': ' ',
 		             'smscounter' : '1',
-		             'counter': length,
+		             'counter': '630',
 		             '/amg/ptk/map/messagebox/formhandlers/MessageFormHandler.to': self.number,
 		             '_D:/amg/ptk/map/messagebox/formhandlers/MessageFormHandler.to': ' ',
 		             '_D:/amg/ptk/map/messagebox/formhandlers/MessageFormHandler.body': ' ',
 		             '/amg/ptk/map/messagebox/formhandlers/MessageFormHandler.body': self.sender+' : '+self.message,
-		             '/amg/ptk/map/messagebox/formhandlers/MessageFormHandler.create.x': '0',
-		             '/amg/ptk/map/messagebox/formhandlers/MessageFormHandler.create.y': '0',
 		             '_D:/amg/ptk/map/messagebox/formhandlers/MessageFormHandler.create': ' ',
+		             '/amg/ptk/map/messagebox/formhandlers/MessageFormHandler.create': 'Wy¿lij',
+		             '/amg/ptk/map/messagebox/formhandlers/MessageFormHandler.create.x': '5',
+		             '/amg/ptk/map/messagebox/formhandlers/MessageFormHandler.create.y': '5',
 		             '_DARGS': '/gear/mapmessagebox/smsform.jsp'}
 		
 		request = urllib2.Request(baseURL + '/portal/map/map/message_box?_DARGS=/gear/mapmessagebox/smsform.jsp')
@@ -102,7 +105,6 @@ class OrangeSMS: # wysylanie do sieci Orange
 		        print 'SMS sended.'
 		except IOError:
 		    print 'SMS not send.'
-				
 		smsy = self.zostalo(result.read())
 		smsy_darmowe = 0
 		if len(smsy) == 1:
